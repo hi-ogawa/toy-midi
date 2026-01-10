5a6c4c4d7531592e255d1903c869f91eab45f3d4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-11 01:27:04 +0900

    chore: cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./e2e",
   webServer: {
-    command: "npm run dev",
+    command: "pnpm dev",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
