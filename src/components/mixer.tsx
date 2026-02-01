93b050cd0c36f4f73b13fd2aaddd675ab975af3d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-01 15:10:00 +0900

    refactor: dedupe metronome icon

--- src/components/mixer.tsx
+++ src/components/mixer.tsx
@@ -1,28 +1,8 @@
 import { MusicIcon, Volume2Icon, VolumeXIcon } from "lucide-react";
 import { useProjectStore } from "../stores/project-store";
+import { MetronomeIcon } from "./icons";
 import { Slider } from "./ui/slider";
 import { Toggle } from "./ui/toggle";
-
-function MetronomeIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      viewBox="0 0 24 24"
-      aria-label="Metronome"
-    >
-      <title>Metronome</title>
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="m14.153 8.188l-.72-3.236a2.493 2.493 0 0 0-4.867 0L5.541 18.566A2 2 0 0 0 7.493 21h7.014a2 2 0 0 0 1.952-2.434l-.524-2.357M11 18l9-13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0"
-      />
-    </svg>
-  );
-}
 
 export function Mixer() {
   const {
