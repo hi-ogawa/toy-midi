483ee3b98ce0d6f924c34e989afabddaf9ffa9ec
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-01 15:04:13 +0900

    revert: remove 0db fader markers

--- src/components/piano-roll.tsx
+++ src/components/piano-roll.tsx
@@ -1038,15 +1038,12 @@ export function PianoRoll() {
                   M
                 </Toggle>
               </div>
-              <div className="relative">
-                <div className="pointer-events-none absolute right-0 top-1/2 h-3 w-px -translate-y-1/2 bg-neutral-500/70" />
-                <Slider
-                  value={[audioVolume * 100]}
-                  onValueChange={([v]) => setAudioVolume(v / 100)}
-                  max={100}
-                  step={1}
-                />
-              </div>
+              <Slider
+                value={[audioVolume * 100]}
+                onValueChange={([v]) => setAudioVolume(v / 100)}
+                max={100}
+                step={1}
+              />
             </div>
             {/* MIDI controls */}
             <div className="flex-1 px-2 pt-3">
@@ -1070,15 +1067,12 @@ export function PianoRoll() {
                   M
                 </Toggle>
               </div>
-              <div className="relative">
-                <div className="pointer-events-none absolute right-0 top-1/2 h-3 w-px -translate-y-1/2 bg-neutral-500/70" />
-                <Slider
-                  value={[midiVolume * 100]}
-                  onValueChange={([v]) => setMidiVolume(v / 100)}
-                  max={100}
-                  step={1}
-                />
-              </div>
+              <Slider
+                value={[midiVolume * 100]}
+                onValueChange={([v]) => setMidiVolume(v / 100)}
+                max={100}
+                step={1}
+              />
             </div>
           </div>
           <div
