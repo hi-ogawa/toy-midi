4ec077d88cb09eaca5b30b55a07fe67ce1f21b72
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-01 15:12:57 +0900

    refactor: dedupe metronome icon (#87)

--- src/components/transport.tsx
+++ src/components/transport.tsx
@@ -15,6 +15,7 @@ import { useWindowEvent } from "../hooks/use-window-event";
 import { audioManager, GM_PROGRAMS } from "../lib/audio";
 import { useProjectStore } from "../stores/project-store";
 import { COMMON_TIME_SIGNATURES, type GridSnap } from "../types";
+import { MetronomeIcon } from "./icons";
 import { Button } from "./ui/button";
 import {
   Command,
@@ -32,27 +33,6 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
-
-function MetronomeIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      viewBox="0 0 24 24"
-      aria-label="Metronome"
-    >
-      <title>Metronome</title>
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="m14.153 8.188l-.72-3.236a2.493 2.493 0 0 0-4.867 0L5.541 18.566A2 2 0 0 0 7.493 21h7.014a2 2 0 0 0 1.952-2.434l-.524-2.357M11 18l9-13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0-2 0"
-      />
-    </svg>
-  );
-}
 
 function formatTimeCompact(seconds: number): string {
   const mins = Math.floor(seconds / 60);
