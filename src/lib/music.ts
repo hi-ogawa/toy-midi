c274a9d19f2c3ff1ad753ecab06f7ec32309bff4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-05-18 16:35:34 +0900

    nit

--- src/lib/music.ts
+++ src/lib/music.ts
@@ -27,16 +27,14 @@ export function isBlackKey(midi: number): boolean {
   return [1, 3, 6, 8, 10].includes(note);
 }
 
-type SnapToGridOptions = {
-  floor?: boolean;
-};
-
 export function snapToGrid(
   value: number,
   gridSize: number,
-  options: SnapToGridOptions = {},
+  options?: {
+    floor?: boolean;
+  },
 ): number {
-  const round = options.floor ? Math.floor : Math.round;
+  const round = options?.floor ? Math.floor : Math.round;
   return round(value / gridSize) * gridSize;
 }
 
