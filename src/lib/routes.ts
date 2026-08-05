757cd25875d9bde3a10fa127be16db23fc2d3d04
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-08-05 23:11:04 +0900

    nit

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,4 +1,5 @@
 import { route } from "@remix-run/fetch-router/routes";
+import { createMultiMatcher } from "@remix-run/route-pattern/match";
 
 export const routes = route({
   home: "/",
@@ -6,3 +7,9 @@ export const routes = route({
   project: "/project/:projectId",
   projectScore: "/project/:projectId/score",
 });
+
+export const routeMatcher = createMultiMatcher<keyof typeof routes>();
+
+for (const [name, route] of Object.entries(routes)) {
+  routeMatcher.add(route.pattern, name as any);
+}
