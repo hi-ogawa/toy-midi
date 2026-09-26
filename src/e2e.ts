0ee1e6cc1297e08a84274cf1fb34c86c50a3da85
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-09-26 14:13:05 +0900

    Revert "test: drop older-save E2E tests"
    This reverts commit 1143abf343079c60fe772ebc45c6505e7135e47c.

--- src/e2e.ts
+++ src/e2e.ts
@@ -4,12 +4,14 @@ import {
   seedProjectV1,
   seedLayoutV1Project,
 } from "./lib/project-storage";
+import { recorderProjectStorage } from "./lib/recorder/project-storage";
 
 const utils = {
   projectStorage,
   seedProjectLegacyV2,
   seedProjectV1,
   seedLayoutV1Project,
+  recorderProjectStorage,
 };
 
 window.__e2e = utils;
